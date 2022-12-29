f426537b5d7b677ac88078d5efa587dbcf0265b0
mubeess <mubarakibrahim2015@gmail.com>
2022-12-29 14:24:59 +0100

    added more flexibility to the server

--- controllers/generateImageController.js
+++ controllers/generateImageController.js
@@ -6,17 +6,17 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const generateImageController=async (req,res) =>{
- const {prompt} =req.body
+ const {prompt,number=1} =req.body
 
 try {
 
  const response = await openai.createImage({
         prompt,
-        n: 1,
+        n:number,
         size:'512x512',
       });
 const image_url = response.data.data[0].url;
-res.json({img:image_url})
+res.json({img:response.data.data})
 
 } catch (error) {
   res.send(error)  
